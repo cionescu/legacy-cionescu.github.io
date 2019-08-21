64c8eabc13f36b067ddfcd1eee84b860d1890ba1
Catalin Ionescu <catalin.ionescu282@gmail.com>
2019-08-21 20:37:45 +0300

    Updates

---
+++ precache-manifest.a0c023cd4cd30a85dc6a3bbd4d059bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e930591f770658950d67162e6309318",
+    "url": "/cionescu/cionescu.github.io/index.html"
+  },
+  {
+    "revision": "6617e6b551a39634bd3a",
+    "url": "/cionescu/cionescu.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "89b1700e19b41f597c18",
+    "url": "/cionescu/cionescu.github.io/static/js/2.f1aecd1d.chunk.js"
+  },
+  {
+    "revision": "6617e6b551a39634bd3a",
+    "url": "/cionescu/cionescu.github.io/static/js/main.6d92608c.chunk.js"
+  },
+  {
+    "revision": "beffab1bc0ccad8b33cc",
+    "url": "/cionescu/cionescu.github.io/static/js/runtime~main.e877c347.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cionescu/cionescu.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cionescu/cionescu.github.io/precache-manifest.a0c023cd4cd30a85dc6a3bbd4d059bbe.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cionescu/cionescu.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
